60c4e80f240936c23ce425312d01589c186ab7ce
Emma <emma_williams1@hotmail.co.uk>
2014-08-12 17:56:07 +0100

    work on logic

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -3,7 +3,12 @@ class RomanNumerals
   #this is a constant as in capitals and applicable here because the rules will not change
 
   def self.convert(number)
-    ROMAN_NUMERALS[number]
+    roman_numeral = ""
+    ROMAN_NUMERALS.each do |k,v|
+      (number / k).times { roman_numeral << v; number = number - k}
+    end
+
+    roman_numeral
  end
  #constant with number passed in 
 end
