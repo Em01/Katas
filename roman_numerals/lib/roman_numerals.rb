9369b7a413443c977845601358ee9de59c3ee73b
Emma <emma_williams1@hotmail.co.uk>
2014-08-12 17:44:25 +0100

    3 tests passing and created a constant

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -1,6 +1,9 @@
 class RomanNumerals
+  ROMAN_NUMERALS = {10 => "X", 5 => "V", 1 => "I"}
+  #this is a constant as in capitals and applicable here because the rules will not change
 
   def self.convert(number)
-    "I"
-  end
+    ROMAN_NUMERALS[number]
+ end
+ #constant with number passed in 
 end
